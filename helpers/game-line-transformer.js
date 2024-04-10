b7cb8795889d0f9ec71bf5aca44601ed328066da
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-04-10 09:35:31 +0000

    feat: add backlog props extractor

--- helpers/game-line-transformer.js
+++ helpers/game-line-transformer.js
@@ -9,6 +9,31 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+/**
+ *
+ * @param {string} store
+ */
+function storeMatch(store) {
+  switch (true) {
+    case store.includes('.steampower'):
+      return 'Steam';
+    case store.includes('.epicgames'):
+      return 'EGS';
+    case store.includes('gog.com'):
+      return 'GOG';
+    case store.includes('ubisoft.'):
+      return 'Ubisoft';
+    case store.includes('.ea.com'):
+      return 'EA';
+    default:
+      throw new Error('Store not found.');
+  }
+}
+
+/**
+ *
+ * @param {string} line
+ */
 function completedGamesPropsExtractor(line) {
   const regex = /\|\s*\[(.*?)\]\((.*?)\)\s*\|\s*(.*?)\s*\|\s*(.*?)\s*\|/;
   const match = line.match(regex);
@@ -18,15 +43,34 @@ function completedGamesPropsExtractor(line) {
     const mainCampaign = match[3];
     const dlc = match[4];
     const newLineFormatter = (finalURL) =>
-      `| ${gameName} | [Steam](${finalURL}) | ${mainCampaign} | ${dlc} | - |`;
+      `| ${gameName} | [${storeMatch(finalURL)}](${finalURL}) | ${mainCampaign} | ${dlc} | - |`;
     return { link, newLineFormatter };
   } else {
     throw new Error(`Expected format:\n | [Game](link) | text | text |\n\nReceived:\n ${line}`);
   }
 }
 
+/**
+ *
+ * @param {string} line
+ */
+function backlogPropsExtractor(line) {
+  const match = line.match(/\|\s*\[(.*?)\]\((.*?)\)\s*\|\s*(.*?)\s*\|/);
+  if (match) {
+    const gameName = match[1];
+    const link = match[2];
+    const notes = match[3];
+
+    const newLineFormatter = (finalURL) =>
+      `| ${gameName} | [${storeMatch(finalURL)}](${finalURL}) | ${notes} |`;
+    return { link, newLineFormatter };
+  } else {
+    throw new Error(`Expected format:\n | [Game](link) | text |\n\nReceived:\n ${line}`);
+  }
+}
+
 rl.question('Enter old line: ', (line) => {
-  const result = completedGamesPropsExtractor(line);
+  const result = backlogPropsExtractor(line);
   if (result) {
     const link = result.link;
 
