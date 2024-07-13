6f468cf101974733b2fb6e9661b24d3dfc84999a
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-07-13 08:51:10 +0000

    feat: copy without new line

--- helpers/lib.js
+++ helpers/lib.js
@@ -4,11 +4,14 @@ const copyCommand =
   process.platform === 'win32'
     ? 'clip'
     : process.platform === 'darwin'
-    ? 'pbcopy'
-    : 'xclip -selection clipboard';
+      ? 'pbcopy'
+      : 'xclip -selection clipboard';
 
 function copyToClipboard(text) {
-  exec(`echo "${text}" | ${copyCommand}`);
+  // For Windows and macOS, the command remains the same as they do not add a new line by default
+  // For Linux with xclip, use `printf` instead of `echo` to avoid adding a new line
+  const command = process.platform === 'linux' ? `printf "%s" "${text}" | ${copyCommand}` : `echo "${text}" | ${copyCommand}`;
+  exec(command);
 }
 
 module.exports = {
