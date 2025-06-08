c942afc07cdc314e20007dfa275dd3d199d76fa1
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-06-08 00:31:14 +0000

    refactor: :recycle: use wl-copy else xclip as fallback

--- helpers/lib.js
+++ helpers/lib.js
@@ -5,11 +5,11 @@ const copyCommand =
     ? 'clip'
     : process.platform === 'darwin'
     ? 'pbcopy'
-    : 'xclip -selection clipboard';
+    : '$(command -v wl-copy >/dev/null 2>&1 && echo "wl-copy" || echo "xclip -selection clipboard")';
 
 function copyToClipboard(text) {
   // For Windows and macOS, the command remains the same as they do not add a new line by default
-  // For Linux with xclip, use `printf` instead of `echo` to avoid adding a new line
+  // For Linux, try wl-copy first (Wayland), fallback to xclip (X11), use `printf` to avoid adding a new line
   const command =
     process.platform === 'linux'
       ? `printf "%s" "${text}" | ${copyCommand}`
