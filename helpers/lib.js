88d7aa37428d5f36d2602d5309a38f87b68d7a64
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-12-08 17:39:59 +0530

    refactor: :recycle: use switch case instead of nested ternary operator

--- helpers/lib.js
+++ helpers/lib.js
@@ -1,11 +1,17 @@
 const { exec } = require("child_process");
-
-const copyCommand =
-  process.platform === "win32"
-    ? "clip"
-    : process.platform === "darwin"
-    ? "pbcopy"
-    : '$(command -v wl-copy >/dev/null 2>&1 && echo "wl-copy" || echo "xclip -selection clipboard")';
+let copyCommand;
+switch (process.platform) {
+  case "win32":
+    copyCommand = "clip";
+    break;
+  case "darwin":
+    copyCommand = "pbcopy";
+    break;
+  default:
+    copyCommand =
+      '$(command -v wl-copy >/dev/null 2>&1 && echo "wl-copy" || echo "xclip -selection clipboard")';
+    break;
+}
 
 function copyToClipboard(text) {
   // For Windows and macOS, the command remains the same as they do not add a new line by default
